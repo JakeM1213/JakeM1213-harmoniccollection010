3377bc4fbe5cda43648fd54ada1e413e39de3f1b
JakeM1213 <144182998+JakeM1213@users.noreply.github.com>
2023-12-05 04:41:36 -0500

    Add files via upload

---
+++ javascript/scripts.js
@@ -0,0 +1,6 @@
+// script.js
+
+function goToAnotherPage() {
+  // Replace 'your_destination_link_here' with the actual destination link
+  window.location.href = 'https://jakem1213.github.io/HarmoniccollectionHome/';
+}
